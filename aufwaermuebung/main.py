75b5805e2723824b67762d7745ec7dfe969d1f97
Matthias Huelser <matthias@huelser.me>
2024-03-03 20:29:26 +0100

    augment aufwaermuebung

--- aufwaermuebung/main.py
+++ aufwaermuebung/main.py
@@ -12,7 +12,7 @@ gt = generate_binary_statements(0.25, 10000)
 
 classifiers = generate_classifiers(sep_low, sep_up, spp_low, spp_up, m)
 
-for classifier in classifiers:
+for index, classifier in enumerate(classifiers):
     results = []
     for _ in range(k):
         result = classifier.get_test_classifications(gt)
@@ -30,9 +30,39 @@ for classifier in classifiers:
     false_negative_fraction = false_negative / len(gt)
     print('')
     print('---------------------------')
+    print(f'classifier {index}')
     print(f'false_positive: {false_positive_fraction}')
     print(f'false_negative: {false_negative_fraction}')
     print(f'sep: {classifier.sep}')
     print(f'spp: {classifier.spp}')
 
 
+for run in range(k):
+    results = []
+    seps = 0
+    spps = 0
+    for classifier in classifiers:
+        result = classifier.get_test_classifications(gt)
+        results.append(result)
+        seps = seps + classifier.sep
+        spps = spps + classifier.spp
+    majority_voted = []
+    for result in zip(*results):
+        vote_result = majority_vote(result)
+        majority_voted.append(vote_result)
+    false_positive = 0
+    false_negative = 0
+    for i in zip(gt, majority_voted):
+        if i[1] == 'K' and i[0] == 'N': false_positive = false_positive + 1
+        if i[1] == 'N' and i[0] == 'K': false_negative = false_negative + 1
+    false_positive_fraction = false_positive / len(gt)
+    false_negative_fraction = false_negative / len(gt)
+    avg_sep = seps / len(classifiers)
+    avg_spp = spps / len(classifiers)
+    print('')
+    print('---------------------------') 
+    print(f'run {run}')
+    print(f'false_positive: {false_positive_fraction}')
+    print(f'false_negative: {false_negative_fraction}')
+    print(f'avg_sep: {avg_sep}')
+    print(f'avg_spp: {avg_spp}')
